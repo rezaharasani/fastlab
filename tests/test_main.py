869a4ecf9bc3812920b0692a2d603942b53b68e0
Reza Harasani <rharasani208@gmail.com>
2025-09-12 11:46:37 +0330

    fixed flake8 too loon string bug

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,7 +9,8 @@ def test_root():
     """Test root function"""
     response = client.get("/")
     assert response.status_code == status.HTTP_200_OK
-    assert response.json() == {"message": "Welcome to deployment with CI/CD pipeline"}
+    message = {"message": "Welcome to deployment with CI/CD pipeline"}
+    assert response.json() == message
 
 
 def test_health():
